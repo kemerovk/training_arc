5b93172a9e21c5423c6e56345246edd19ff22980
Tsoy_Kirill <tsoykirya@gmail.com>
2025-02-02 23:43:19 +0300

    dsfsdf

---
+++ src/main/java/me/project/training_arc/TrainingArcApplication.java
@@ -0,0 +1,13 @@
+package me.project.training_arc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingArcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingArcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/project/training_arc/TrainingArcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.project.training_arc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingArcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
